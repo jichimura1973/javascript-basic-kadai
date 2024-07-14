f38422a0f4b2d09c2bd287f8b7437f55e3335787
jichimura1973 <jichimura1973@gmail.com>
2024-07-14 14:27:20 +0900

    課題21を登録

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () =>{
+    // textというidを持つHTML要素を取得し、定数に代入する
+    const h2 = document.getElementById('text');
+
+    setTimeout(() =>{
+        // ボタンが押された時「ボタンをクリックしました」と表示する。
+        h2.textContent = 'ボタンをクリックしました';
+    },2000);
+    
+});
